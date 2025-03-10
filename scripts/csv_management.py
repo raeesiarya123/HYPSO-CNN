779556e8f772658d6f60ab1503edda0ee9bc8ab4
raeesiarya123 <aryar@stud.ntnu.no>
2025-03-10 10:33:04 +0100

    Changes to CSV management

--- scripts/csv_management.py
+++ scripts/csv_management.py
@@ -62,13 +62,10 @@ def find_dat_and_bip_files():
     return dat_files_paths, bip_files_paths
 
 
-def create_csv_file():
-    """
-    Creates a CSV file ('files.csv') that maps .dat files to their corresponding .bip@ files.
+"""
+OLD VERSION
 
-    - Calls find_dat_and_bip_files() to retrieve paths.
-    - Writes the file paths into 'files.csv' with headers: ["dat_files", "bip_files"].
-    """
+def create_csv_file():
 
     dat_files_paths, bip_files_paths = find_dat_and_bip_files()
 
@@ -77,8 +74,50 @@ def create_csv_file():
         writer.writerow(["dat_files", "bip_files"])
         for i in range(len(dat_files_paths)):
             writer.writerow([dat_files_paths[i], bip_files_paths[i]])
+"""
 
-#create_csv_file()
+def create_csv_file():
+
+    """
+    Creates a CSV file ('files.csv') that maps .dat files to their corresponding .bip@ files.
+
+    - Calls find_dat_and_bip_files() to retrieve paths.
+    - Writes the file paths into 'train_files.csv' and 'evaluate_files.csv' with headers: ["dat_files", "bip_files"].
+    """
+
+    dat_files_paths, bip_files_paths = find_dat_and_bip_files()
+
+    if len(dat_files_paths) == 1:
+        print("a")
+    elif len(dat_files_paths) > 1:
+        split_index = max(1, int(len(dat_files_paths) * 0.8))
+        train_dat_files = dat_files_paths[:split_index]
+        eval_dat_files = dat_files_paths[split_index:]
+        train_bip_files = bip_files_paths[:split_index]
+        eval_bip_files = bip_files_paths[split_index:]
+
+        print(len(train_bip_files))
+        print(len(train_dat_files))
+
+        with open('train_files.csv', mode='w') as train_file:
+            writer = csv.writer(train_file)
+            writer.writerow(["dat_files", "bip_files"])
+            for i in range(len(train_dat_files)):
+                writer.writerow([train_dat_files[i], train_bip_files[i]])
+
+        with open('evaluate_files.csv', mode='w') as eval_file:
+            writer = csv.writer(eval_file)
+            writer.writerow(["dat_files", "bip_files"])
+            for i in range(len(eval_dat_files)):
+                writer.writerow([eval_dat_files[i], eval_bip_files[i]])
+    else:
+        with open('train_files.csv', mode='w') as train_file:
+            writer = csv.writer(train_file)
+            writer.writerow(["dat_files", "bip_files"])
+            for i in range(len(dat_files_paths)):
+                writer.writerow([dat_files_paths[i], bip_files_paths[i]])
+
+create_csv_file()
 
 def read_csv_file(csv_file_with_path):
     """
